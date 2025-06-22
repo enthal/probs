1699df08ce505916ef5836472b49b4a7911fd9e3
Tim James <probs@tjames.com>
2025-06-22 12:27:33 -0700

    merge-two-sorted-lists: actually splice

--- merge-two-sorted-lists/Solution.java
+++ merge-two-sorted-lists/Solution.java
@@ -3,39 +3,37 @@
 class Solution {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // assume: "sorted linked lists", input list mods ok but not required: "splicing together the nodes",
+        // assume: "sorted linked lists", input list mods expected: "splicing together the nodes",
         // case: one list is null: take the other
         // Until one list ends: take from lowest-val head.
         // Then take the remaining from the other list.
         ListNode head = null, cur = null;
         for (; ; ) {
-            ListNode made;
+            ListNode got;
             if (list1 == null && list2 == null) {
                 return head;
             } else if (list2 == null) {
-                made = list1;
+                got = list1;
                 list1 = null;
             } else if (list1 == null) {
-                made = list2;
+                got = list2;
                 list2 = null;
             } else if (list1.val < list2.val) {
-                made = new ListNode(list1.val);
+                got = list1;
                 list1 = list1.next;
             } else {
-                made = new ListNode(list2.val);
+                got = list2;
                 list2 = list2.next;
             }
 
             if (head == null) {
-                head = made;
+                head = got;
             }
             if (cur != null) {
-                cur.next = made;
+                cur.next = got;
             }
 
-            cur = made;
+            cur = got;
         }
     }
-
-
 }
