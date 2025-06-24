302a6a983fd3f58f555a3ef9637a1461ec9130a1
Tim James <probs@tjames.com>
2025-06-23 21:17:37 -0700

    3sum: O(n^2)ish, but O(1) for same 3 numbers are duplicated many times

--- src/main/java/_3sum/Solution.java
+++ src/main/java/_3sum/Solution.java
@@ -6,7 +6,40 @@ import java.util.stream.IntStream;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        return threeSum_O_nSquared(nums);
+        return threeSum_O_nSquared_shrunk(nums);
+    }
+
+    public List<List<Integer>> threeSum_O_nSquared_shrunk(int[] nums) {
+        var indexTriples = new ArrayList<Set<Integer>>();
+        var indexListsByNum = new HashMap<Integer, List<Integer>>();
+        for (var i=0; i<nums.length; i++) {
+            var indexList = indexListsByNum.computeIfAbsent(nums[i], ii -> new ArrayList<>());
+            if (indexList.size()<3) indexList.add(i);  // Optimization: Can't use more than 3.
+        }
+
+        // O(m^2) where m is the number of distinct numbers in nums
+        indexListsByNum.forEach((iNum, iIndexes) ->
+                iIndexes.forEach(i -> // limited to size=3 above
+                        indexListsByNum.forEach((jNum, jIndexes) ->
+                                jIndexes.forEach(j -> { // limited to size=3 above
+                                    if (i.equals(j)) return;
+                                    var kNum = -(iNum + jNum);
+                                    var ks = indexListsByNum.get(kNum);
+                                    if (ks != null) {
+                                        for (var k : ks) { // limited to size=3 above
+                                            if (!i.equals(k) && !j.equals(k)) {
+                                                indexTriples.add(Set.of(i, j, k));
+                                            }
+                                        }
+                                    }
+                                })))
+        );
+
+        return indexTriples.stream()
+                .map(t->t.stream().map(i->nums[i]).sorted().toList())
+                .distinct()
+                // .sorted()
+                .toList();
     }
 
     public List<List<Integer>> threeSum_O_nSquared(int[] nums) {
